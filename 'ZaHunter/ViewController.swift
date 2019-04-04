b305ff94320c29bdd9ac0a8dad0bbf964801015a
Julio Rivera <jrivera0325@jh-mba158-16.local>
2019-04-04 11:17:58 -0500

    MVP done

--- 'ZaHunter/ViewController.swift
+++ 'ZaHunter/ViewController.swift
@@ -7,16 +7,47 @@
 //
 
 import UIKit
+import MapKit
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    var currentLocation: CLLocation!
+    var pizzas: [MKMapItem] = []
+    var initialRegion: MKCoordinateRegion!
+    var isInitialMapLoad = true
+    
+    func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
+        if isInitialMapLoad {
+            initialRegion = MKCoordinateRegion(center: mapView.centerCoordinate, span: mapView.region.span)
+            isInitialMapLoad = false
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
         
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        
+        mapView.delegate = self
     }
 
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currentLocation = locations[0]
+    }
+    
+    
     @IBAction func zoomButton(_ sender: UIBarButtonItem) {
-        
+        let center = currentLocation.coordinate
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: center, span: span)
+        mapView.setRegion(region, animated: true)
     }
     
 
